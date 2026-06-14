db0aec57a97af7d5b8f9d25ec89a6b3004ef2a8f
Job Roman Barasa <jobb.roman@gmail.com>
2026-06-14 15:39:08 +0300

    Add files via upload

--- Week_5/rsagen.py
+++ Week_5/rsagen.py
@@ -12,3 +12,43 @@ public_key = private_key.public_key()
 print("RSA Key Pair Generated Successfully!")
 print("Private Key:", private_key)
 print("Public Key:", public_key)
+
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+message = b"Secure Communication"
+
+encrypted = public_key.encrypt(
+    message,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+print("Encrypted Message:")
+print(encrypted)
+
+decrypted = private_key.decrypt(
+    encrypted,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+print("Decrypted Message:")
+print(decrypted.decode())
+
+print("\nSecure Message Transmission")
+print("Original Message:", message.decode())
+print("Encrypted Message Sent.")
+print("Private Key Used for Decryption.")
+print("Recovered Message:", decrypted.decode())
+
+if decrypted == message:
+    print("RSA Validation Successful!")
+else:
+    print("RSA Validation Failed!")
